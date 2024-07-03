94242ccce2ba143cf2da9d1d0b440de3602f8f1b
Cristian Beraha <cristian@cloudops.works>
2024-07-03 15:54:36 -0300

    chore: IAM attachments will give problems at some point

--- iam-default.tf
+++ iam-default.tf
@@ -20,6 +20,15 @@ data "aws_iam_policy_document" "instance_role" {
 resource "aws_iam_role" "instance_role" {
   name               = "eb-ec2-role-${local.system_name}"
   assume_role_policy = data.aws_iam_policy_document.instance_role.json
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier",
+    "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier",
+    "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+  ]
   tags               = local.all_tags
 }
 
@@ -27,39 +36,4 @@ resource "aws_iam_instance_profile" "instance_role" {
   name = aws_iam_role.instance_role.name
   role = aws_iam_role.instance_role.id
   tags = local.all_tags
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_ro" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_for_ssm" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_role_policy_attachment" "s3_full_access" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "multi_docker" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
-}
-
-resource "aws_iam_role_policy_attachment" "web_tier" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-}
-
-resource "aws_iam_role_policy_attachment" "worker_tier" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
-}
-
-resource "aws_iam_role_policy_attachment" "cloudwatch_full_access" {
-  role       = aws_iam_role.instance_role.id
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
